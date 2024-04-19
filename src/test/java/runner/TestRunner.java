4a8099e145ee51327b58144d07bb6b970f9f4502
AZhizhov <vsreg@hotmail.com>
2024-04-19 20:51:13 +0500

    jenkins правки

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import static io.cucumber.core.options.Constants.*;
 @IncludeEngines("cucumber") // объясняем Suite что это cucumber и мы будем запускать feature файл, передаем cucumber
 @ConfigurationParameters({
         //@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
-        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/feature"),
+        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/Feature"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.ibs.steps"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 })
